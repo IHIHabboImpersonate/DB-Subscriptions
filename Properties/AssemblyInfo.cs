4e17b3bad9d681522b03acdcb838c83f3f198ccf
Chris Chenery <Cecer1@cecer1.com>
2011-07-16 19:53:46 +0100

    Initial commit

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("DB-Subscriptions")]
+[assembly: AssemblyDescription("The database structure for subscriptions")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Bluedot")]
+[assembly: AssemblyProduct("DB-Subscriptions")]
+[assembly: AssemblyCopyright("Copyright © Bluedot 2011")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("003cc2bc-ff2b-45bc-bcbd-1b175fd14de1")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ subscriptions/Subscription.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IHI.Database
+{
+    public class Subscription
+    {
+        private int fHabboID;
+        public virtual int habbo_id
+        {
+            get { return this.fHabboID; }
+            set
+            {
+                this.fHabboID = value;
+            }
+        }
+        
+        private int fTotalBought;
+        public virtual int total_bought
+        {
+            get { return this.fTotalBought; }
+            set
+            {
+                this.fTotalBought = value;
+            }
+        }
+        private int fSkippedLength;
+        public virtual int skipped_length
+        {
+            get { return this.fSkippedLength; }
+            set
+            {
+                this.fSkippedLength = value;
+            }
+        }
+        private int fPausedStart;
+        public virtual int paused_start
+        {
+            get { return this.fPausedStart; }
+            set
+            {
+                this.fPausedStart = value;
+            }
+        }
+    }
+}
